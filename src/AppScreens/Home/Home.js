70246a896e42bd0bfa428942bc46bac75df382fe
Matheus Nascimento Soares Marques de Lima <matheus.marques_96@hotmail.com>
2022-11-02 03:08:03 -0300

    add search section and header destaques

--- src/AppScreens/Home/Home.js
+++ src/AppScreens/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Dimensions, StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import { Dimensions, StyleSheet, TextInput, View, ScrollView, Image } from "react-native";
+
 import GlobalFooter from "../../Footers/GlobalFooter";
 import GlobalHeader from "../../Headers/GlobalHeader";
 
@@ -8,12 +9,41 @@ const Home = ({ AppState }) => {
     const dimensions = Dimensions.get('window');
     const imageWidth = Math.round(dimensions.width*0.3);
 
-    var posters = allcards.map(card => card.cardImg)
+    
+    var posters = allcards.map(card => card.cardImg);
     return (
         <View style={styles.screen}>
             <GlobalHeader/>
             <View style={styles.body}>
-                <ScrollView horizontal={true} marginLeft={-imageWidth-20} marginRight={-imageWidth-20}>
+                <View style={styles.header}>
+                    <Image
+                        style={styles.imgLeft}
+                        source={require("../../../assets/legendas-tv-small-logo.png")}
+                    />
+                    <View style={styles.searchSection}>
+                        <TextInput 
+                                style={styles.searchInput}
+                                cursorColor= '#4885C4'
+                                selectionColor= '#4885C4'
+                            />
+                        <Image
+                            source={require('../../../assets/search.png')}
+                            style={styles.searchIcon}
+                        />
+      
+                    </View>
+                    <Image
+                        style={styles.imgRigth}
+                        source={require("../../../assets/alert.png")}
+                    />
+                </View>
+                <Image
+                    marginTop={20}
+                    resizeMode={'contain'}
+                    style={{ width: dimensions.width }}
+                    source={require("../../../assets/destaques.png")}
+                />
+                <ScrollView horizontal={true} marginLeft={-imageWidth} marginRight={-imageWidth-15} marginTop={5}>
                 {
                     posters.map((poster, i) => {
                         return (
@@ -23,7 +53,8 @@ const Home = ({ AppState }) => {
                                 style={{ width: imageWidth }}
                                 key={i}
                                 source={poster}
-                                margin={10}
+                                marginRight={15}
+                                borderRadius={20}
                             />
                         );
                     })
@@ -44,7 +75,49 @@ const styles = StyleSheet.create({
     body: {
         flex: 1,
         marginTop: 50
-    }
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:15,
+        marginRight:10,
+        marginLeft:5,
+        height: 40
+    },
+    searchSection: {
+        flex:0.8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 40,
+        marginLeft: 5,
+        marginRight: 10,
+        borderWidth: 1,
+        padding: 5,
+        borderRadius: 20,
+        borderColor: '#4885C4',
+    },
+    searchIcon: {
+        height: 30,
+        width: 30,
+        resizeMode: 'stretch',
+        alignItems: 'center',
+    },
+    searchInput: {
+        flex:1,
+    },
+    imgLeft: {
+        flex:0.2,
+        resizeMethod: 'resize',
+        resizeMode: 'contain',
+    },
+    imgRigth: {
+        flex:0.1,
+        resizeMethod: 'resize',
+        resizeMode: 'contain'
+    },
+
   });
   
 
